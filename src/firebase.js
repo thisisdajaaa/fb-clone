5f0a21eab7e9139f51710c8cc1316e6def0d841c
Dann Anthony Astillero <daja@Danns-MacBook-Pro.local>
2021-04-21 22:50:14 +0800

    feat: initial commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCaCNY8UFnDYJqIDhl271Vt_HxqH8csFRM",
+  authDomain: "fb-clone-cb429.firebaseapp.com",
+  projectId: "fb-clone-cb429",
+  storageBucket: "fb-clone-cb429.appspot.com",
+  messagingSenderId: "594495648924",
+  appId: "1:594495648924:web:6a3a4ab81c54c63af92959",
+  measurementId: "G-16D797FGFM",
+};
+
+export { firebaseConfig };
